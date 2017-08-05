719d8870d53f7aac10b47a517927edc8af85384b
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2017-08-05 15:54:04 +0530

    positive.c
    positive

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the number:");
+scanf("%d",&num);
+if(num<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is positive")
+}
+getch();
+}
